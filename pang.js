4823b2045485d057aa7da39a6e24cd3822ebe557
laxopy <39135816+laxopy@users.noreply.github.com>
2019-04-03 20:48:46 +0200

    Add files via upload

---
+++ pang.js
@@ -0,0 +1,49 @@
+var circleM = {
+			x : 10,
+			y : 200,
+			rad :10
+  
+}
+
+var shoot = {
+			x1 : event.screenX,
+			y1 : event.screenY,
+			y2 : y1-10
+  
+}
+
+var dir = 1;
+var x1;
+function showCoords(event) {
+  var x1 = event.clientX;
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(255,0,255);
+  circle(circleM.x,circleM.y,circleM.rad);
+  
+if (circleM.x < (width - circleM.rad) && dir == 1){
+  circleM.x ++
+} else if (circleM.x == (width - circleM.rad) && dir == 1){
+  dir = 2;
+} else if (dir == 2 && circleM.x) {
+  circleM.x -= 1;
+} else {
+  dir = 1;
+}
+//line (200,400,200,395)
+line (shoot.x1,shoot.y1,shoot.x1,shoot.y2)
+shoot.y1 = shoot.y1 - 1;
+shoot.y2 = shoot.y2 - 1;
+print (circleM.x);
+print (dir);
+
+
+//print(x1)
+
+}
